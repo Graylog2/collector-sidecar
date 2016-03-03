e85a0593e838362605d4176b987d07b65ebf0783
Bernd Ahlers <bernd@graylog.com>
2016-03-03 13:15:23 +0100

    Change collectors URL from /system/collectors to /collectors

--- api/graylog.go
+++ api/graylog.go
@@ -57,7 +57,7 @@ func UpdateRegistration(context *context.Ctx) {
 	h.Add("X-Graylog-Collector-Version", util.CollectorVersion)
 
 	r := napping.Request{
-		Url:     context.ServerUrl.String() + "/plugins/org.graylog.plugins.collector/system/collectors/" + context.CollectorId,
+		Url:     context.ServerUrl.String() + "/plugins/org.graylog.plugins.collector/collectors/" + context.CollectorId,
 		Method:  "PUT",
 		Payload: registration,
 		Header:  &h,
