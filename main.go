31fcf7ba05f8f4403cc35fe4d956b5347d24389d
Marius Sturm <marius@graylog.com>
2016-03-04 13:57:37 +0100

    make ini file configurable

--- main.go
+++ main.go
@@ -31,16 +31,37 @@ import (
 
 	// importing backend packages to ensure init() is called
 	_ "github.com/Graylog2/sidecar/backends/nxlog"
+	"fmt"
 )
 
 var log = util.Log()
 
 func main() {
-	sidecarConfigurationFile := ""
-	if runtime.GOOS == "windows" {
-		sidecarConfigurationFile = filepath.Join("C:\\", "Program Files (x86)", "sidecar", "sidecar.ini")
-	} else {
-		sidecarConfigurationFile = filepath.Join("/etc", "sidecar", "sidecar.ini")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: sidecar [OPTIONS] [CONFIGURATION FILE]\n")
+		fmt.Fprintf(os.Stderr, "OPTIONS can be:\n")
+		flag.PrintDefaults()
+	}
+	var (
+		svcFlagParam           = flag.String("service", "", "Control the system service")
+		backendParam           = flag.String("backend", "nxlog", "Set the collector backend")
+		collectorPathParam     = flag.String("collector-path", "/usr/bin/nxlog", "Path to collector installation")
+		collectorConfPathParam = flag.String("collector-conf-path", "/etc/graylog/sidecar/generated/nxlog.conf", "File path to the rendered collector configuration")
+		serverUrlParam         = flag.String("server-url", "http://127.0.0.1:12900", "Graylog server URL")
+		nodeIdParam            = flag.String("node-id", "graylog-sidecar", "Collector identification string")
+		collectorIdParam       = flag.String("collector-id", "file:/etc/graylog/sidecar/collector-id", "UUID used for collector registration")
+		tagsParam              = flag.String("tags", "", "Comma separated tag list")
+		logPathParam           = flag.String("log-path", "/var/log/graylog/sidecar", "Directory for collector output logs")
+	)
+
+	flag.Parse() // dummy parse to access flag.Arg(n)
+	sidecarConfigurationFile := flag.Arg(0)
+	if sidecarConfigurationFile == "" {
+		if runtime.GOOS == "windows" {
+			sidecarConfigurationFile = filepath.Join("C:\\", "Program Files (x86)", "graylog", "sidecar", "sidecar.ini")
+		} else {
+			sidecarConfigurationFile = filepath.Join("/etc", "graylog", "sidecar", "sidecar.ini")
+		}
 	}
 	if _, err := os.Stat(sidecarConfigurationFile); os.IsNotExist(err) {
 		log.Error("Can not open sidecar configuration " + sidecarConfigurationFile)
@@ -52,18 +73,6 @@ func main() {
 		Filename:  sidecarConfigurationFile,
 		EnvPrefix: "SIDECAR_",
 	})
-
-	var (
-		svcFlagParam           = flag.String("service", "", "Control the system service")
-		backendParam           = flag.String("backend", "nxlog", "Set the collector backend")
-		collectorPathParam     = flag.String("collector-path", "/usr/bin/nxlog", "Path to collector installation")
-		collectorConfPathParam = flag.String("collector-conf-path", "/etc/sidecar/generated/nxlog.conf", "File path to the rendered collector configuration")
-		serverUrlParam         = flag.String("server-url", "http://127.0.0.1:12900", "Graylog server URL")
-		nodeIdParam            = flag.String("node-id", "graylog-sidecar", "Collector identification string")
-		collectorIdParam       = flag.String("collector-id", "file:/etc/sidecar/collector-id", "UUID used for collector registration")
-		tagsParam              = flag.String("tags", "", "Comma separated tag list")
-		logPathParam           = flag.String("log-path", "/var/log/sidecar", "Directory for collector output logs")
-	)
 	conf.ParseAll()
 
 	expandedCollectorPath := util.ExpandPath(*collectorPathParam)
