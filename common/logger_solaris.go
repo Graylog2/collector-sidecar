b3d102ab666a84b8ed99443aca8e99018ef98c69
Marius Sturm <marius@graylog.com>
2016-05-23 16:34:07 +0200

    add Solaris build

---
+++ common/logger_solaris.go
@@ -0,0 +1,23 @@
+package common
+
+import (
+	"log/syslog"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/Sirupsen/logrus/hooks/syslog"
+)
+
+var log = logrus.New()
+
+func init() {
+	// initialize logging
+	hook, err := logrus_syslog.NewSyslogHook("", "", syslog.LOG_INFO, "")
+
+	if err == nil {
+		log.Hooks.Add(hook)
+	}
+}
+
+func Log() *logrus.Logger {
+	return log
+}
