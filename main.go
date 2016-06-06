4a1283a94eb4c684361b40d2c4f317a9c9cfe579
Marius Sturm <marius@graylog.com>
2016-06-06 16:43:33 +0200

    remove topbeat backend

--- main.go
+++ main.go
@@ -33,7 +33,6 @@ import (
 
 	// importing backend packages to ensure init() is called
 	_ "github.com/Graylog2/collector-sidecar/backends/beats/filebeat"
-	_ "github.com/Graylog2/collector-sidecar/backends/beats/topbeat"
 	_ "github.com/Graylog2/collector-sidecar/backends/beats/winlogbeat"
 	_ "github.com/Graylog2/collector-sidecar/backends/nxlog"
 	_ "github.com/Graylog2/collector-sidecar/daemon"
