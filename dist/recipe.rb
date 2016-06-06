bf4a3ab72dc8b0ea23277ea7cf125d99666c9cb4
Marius Sturm <marius@graylog.com>
2016-06-06 16:48:16 +0200

    next development iteration

--- dist/recipe.rb
+++ dist/recipe.rb
@@ -2,7 +2,7 @@ class GraylogSidecar < FPM::Cookery::Recipe
   description 'Graylog collector sidecar'
 
   name     'collector-sidecar'
-  version  '0.0.8'
+  version  '0.0.9'
   revision '1'
   homepage 'https://graylog.org'
   arch     'amd64'
--- dist/recipe32.nsi
+++ dist/recipe32.nsi
@@ -3,7 +3,7 @@
  
  
   Name "Graylog Collector Sidecar"
-  !define VERSION "0.0.8"
+  !define VERSION "0.0.9"
   !define MUI_FILE "savefile"
   !define MUI_BRANDINGTEXT "Graylog Collector Sidecar v${VERSION}"
   CRCCheck On
