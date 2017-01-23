7e2980b9fda10c58f636bfeff6870dc2b621ecfd
Dennis Oelkers <dennis@graylog.com>
2017-01-23 16:55:25 +0100

    Adding build task and target for FreeBSD.

--- services/control_handler_unix.go
+++ services/control_handler_unix.go
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
 
-// +build darwin linux solaris
+// +build darwin linux solaris freebsd
 
 package services
 
