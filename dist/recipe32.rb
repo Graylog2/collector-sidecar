b33a799bf0ebde4c01894bdf853e3bb1fd8267f3
Sami Pajunen <sami.pajunen@digia.com>
2017-08-25 12:13:07 +0000

    Custom package information for pulssi fork of sidecar.

--- dist/recipe32.rb
+++ dist/recipe32.rb
@@ -3,16 +3,16 @@ require_relative 'tools'
 class GraylogSidecar < FPM::Cookery::Recipe
   description 'Graylog collector sidecar'
 
-  name     'collector-sidecar'
+  name     'collector-sidecar-pulssi'
   version  data.version
   revision data.revision
-  homepage 'https://graylog.org'
+  homepage 'https://github.com/digiapulssi/collector-sidecar'
   arch     'i386'
 
   source   "file:../../build/#{version}/linux/386/graylog-collector-sidecar"
 
-  maintainer 'Graylog, Inc. <hello@graylog.org>'
-  vendor     'graylog'
+  maintainer 'Sami Pajunen <sami.pajunen@digia.com>'
+  vendor     'Digia'
   license    'GPLv3'
 
   config_files '/etc/graylog/collector-sidecar/collector_sidecar.yml'
