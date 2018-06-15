71644529db4769d4de4d74ed4818b46f0bf46b0a
Bernd Ahlers <bernd@graylog.com>
2018-06-15 18:44:44 +0200

    Update to beats 6.3.0

--- dist/fetch_collectors.sh
+++ dist/fetch_collectors.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ARCHS=( x86 x86_64 )
-FILEBEAT_VERSION=6.1.2
-WINLOGBEAT_VERSION=6.1.2
+FILEBEAT_VERSION=6.3.0
+WINLOGBEAT_VERSION=6.3.0
 
 # $1: beat name
 # $2: beat operating system
