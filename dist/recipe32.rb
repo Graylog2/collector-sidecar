e18b40bb149a74a5d1f7d6dcaf15da6428044798
Marco Pfatschbacher <marco@graylog.com>
2018-10-30 16:45:11 +0100

    Install empty module directory to suppress filebeat warning
    We don't use modules and these errors are harmless,
    but less errors are better.
    
    Fixes #303

--- dist/recipe32.rb
+++ dist/recipe32.rb
@@ -25,6 +25,7 @@ class GraylogSidecar < FPM::Cookery::Recipe
   def install
     bin.install 'graylog-sidecar'
     lib('graylog-sidecar').install '../../collectors/filebeat/linux/x86/filebeat'
+    lib('graylog-sidecar/module').mkdir
     etc('graylog/sidecar').install '../../../sidecar-example.yml', 'sidecar.yml'
     var('lib/graylog-sidecar/generated').mkdir
     var('log/graylog-sidecar').mkdir
