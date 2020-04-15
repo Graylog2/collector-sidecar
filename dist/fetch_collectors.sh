52e456c95ccdf8c9fc78642fe1ea63a969a8c55e
Marco Pfatschbacher <marco@graylog.com>
2020-04-15 10:17:37 +0200

    Update filebeat and winlogbeat to 7.6.2 (#391)

--- dist/fetch_collectors.sh
+++ dist/fetch_collectors.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ARCHS=( x86 x86_64 )
-FILEBEAT_VERSION=6.4.2
-WINLOGBEAT_VERSION=6.4.2
+FILEBEAT_VERSION=7.6.2
+WINLOGBEAT_VERSION=7.6.2
 
 # $1: beat name
 # $2: beat operating system
