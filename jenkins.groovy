70db7e2d8f5cdce7d3f3065f5b0fab12c5a3766d
Donald Morton <donald.morton@graylog.com>
2020-06-25 09:21:00 -0500

    Clean up old builds and artifacts more agressively.

--- jenkins.groovy
+++ jenkins.groovy
@@ -10,7 +10,7 @@ pipeline
 
    options
    {
-      buildDiscarder logRotator(artifactDaysToKeepStr: '30', artifactNumToKeepStr: '100', daysToKeepStr: '30', numToKeepStr: '100')
+      buildDiscarder logRotator(artifactDaysToKeepStr: '30', artifactNumToKeepStr: '10', daysToKeepStr: '30', numToKeepStr: '10')
       timestamps()
       withAWS(region:'eu-west-1', credentials:'aws-key-releases')
    }
