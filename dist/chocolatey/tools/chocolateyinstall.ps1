a524b7818ac5f7c0399c40e603f779396b03441c
Donald Morton <donald.morton@graylog.com>
2021-03-01 16:06:40 -0600

    Minor updates.

--- dist/chocolatey/tools/chocolateyinstall.ps1
+++ dist/chocolatey/tools/chocolateyinstall.ps1
@@ -6,17 +6,14 @@ $packageArgs = @{
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = 'https://downloads.graylog.org/releases/graylog-collector-sidecar/1.0.2/graylog_sidecar_installer_1.0.2-1.exe'
-  url64bit      = 'https://downloads.graylog.org/releases/graylog-collector-sidecar/1.0.2/graylog_sidecar_installer_1.0.2-1.exe'
   softwareName  = 'graylog-sidecar*'
 
   # Checksums are now required as of 0.10.0.
   # To determine checksums, you can get that from the original site if provided.
   # You can also use checksum.exe (choco install checksum) and use it
   # e.g. checksum -t sha256 -f path\to\file
-  checksum      = ''
+  checksum      = 'e6eac94e512599ffb5860d29782d619c8a78fd5fd3b39f8023049cace0f7868a'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
-  checksum64    = ''
-  checksumType64= 'sha256' #default is checksumType
   silentArgs   = '/S'
 }
 
