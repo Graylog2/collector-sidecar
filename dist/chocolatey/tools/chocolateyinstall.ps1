65f503fbb3e66ad6ad5ae2fff16fed592eed149c
Donald Morton <donald.morton@graylog.com>
2021-03-03 13:58:54 -0600

    Fix versions.

--- dist/chocolatey/tools/chocolateyinstall.ps1
+++ dist/chocolatey/tools/chocolateyinstall.ps1
@@ -13,7 +13,7 @@ $packageArgs = @{
   # You can also use checksum.exe (choco install checksum) and use it
   # e.g. checksum -t sha256 -f path\to\file
   checksum      = 'e6eac94e512599ffb5860d29782d619c8a78fd5fd3b39f8023049cace0f7868a'
-  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+  checksumType  = 'sha256'
   silentArgs   = '/S'
 }
 
