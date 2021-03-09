ab8c0de3e53a44e51bd13509e8b1ff06c8f01481
Donald Morton <donald.morton@graylog.com>
2021-03-09 12:57:55 -0600

    Revert #391 to test windows service.

--- dist/fetch_collectors.sh
+++ dist/fetch_collectors.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ARCHS=( x86 x86_64 )
-FILEBEAT_VERSION=7.6.2
-WINLOGBEAT_VERSION=7.6.2
+FILEBEAT_VERSION=6.4.2
+WINLOGBEAT_VERSION=6.4.2
 
 # $1: beat name
 # $2: beat operating system
@@ -43,4 +43,3 @@ do
   download_beat "filebeat" "windows" ${FILEBEAT_VERSION} ${ARCH}
   download_beat "winlogbeat" "windows" ${WINLOGBEAT_VERSION} ${ARCH}
 done
-
