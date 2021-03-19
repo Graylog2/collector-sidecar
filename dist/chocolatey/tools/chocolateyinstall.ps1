cd538a158832ec48f257598fe44185ccfff84c4c
Donald Morton <donald.morton@graylog.com>
2021-03-19 08:45:35 -0500

    Fix exe sha.

--- dist/chocolatey/tools/chocolateyinstall.ps1
+++ dist/chocolatey/tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   url           = 'https://downloads.graylog.org/releases/graylog-collector-sidecar/1.1.0/graylog_sidecar_installer_1.1.0-1.exe'
   softwareName  = 'graylog-sidecar*'
-  checksum      = '55693ad815021985d8ecc733e918e285dc892233035b6dcac9c11b480bf1af42'
+  checksum      = '08ee296fa6970fec2026d5956bdfcf9eb622dba92c8e5f811ddf3a765041b810'
   checksumType  = 'sha256'
   silentArgs   = '/S'
 }
