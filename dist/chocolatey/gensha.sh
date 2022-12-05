dc4c5f5c1e8cccbd5e7349d43c382b57a68fadde
Bernd Ahlers <bernd@graylog.com>
2022-12-05 16:57:14 +0100

    Fix Chocolatey installer script
    - Set -e
    - Use the correct filename

--- dist/chocolatey/gensha.sh
+++ dist/chocolatey/gensha.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 # gensha.sh - Generate sha256 file for Chocolatey package and update chocolateyinstall.ps1 with the correct version and checksum. 
 
-COLLECTOR_VERSION=$1
-COLLECTOR_REVISION=$2
-COLLECTOR_VERSION_SUFFIX=$3
+set -eo pipefail
 
-if [[ ${COLLECTOR_VERSION_SUFFIX} == "-SNAPSHOT" ]]; then
-  COLLECTOR_CHECKSUM=$(sha256sum dist/pkg/graylog_sidecar_installer_${COLLECTOR_VERSION}-${COLLECTOR_REVISION}.SNAPSHOT.exe | cut -d" " -f1)
-else
-  COLLECTOR_CHECKSUM=$(sha256sum dist/pkg/graylog_sidecar_installer_${COLLECTOR_VERSION}-${COLLECTOR_REVISION}.exe | cut -d" " -f1)
-fi
+COLLECTOR_VERSION="$1"
+COLLECTOR_INSTALLER_VERSION="$2"
+
+COLLECTOR_CHECKSUM="$(sha256sum dist/pkg/graylog_sidecar_installer_${COLLECTOR_INSTALLER_VERSION}.exe | cut -d" " -f1)"
 
 root_url="https://downloads.graylog.org/releases/graylog-collector-sidecar"
-version_url="${root_url}/${COLLECTOR_VERSION}/graylog_sidecar_installer_${COLLECTOR_VERSION}-${COLLECTOR_REVISION}.exe"
+version_url="${root_url}/${COLLECTOR_VERSION}/graylog_sidecar_installer_${COLLECTOR_INSTALLER_VERSION}.exe"
 
 sed -e "s,%%CHECKSUM%%,$COLLECTOR_CHECKSUM,g" \
 	-e "s,%%URL%%,$version_url,g" \
