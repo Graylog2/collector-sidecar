952e87f42c07e081d8a2285bc0a7189f3b99164b
Marco Pfatschbacher <marco@graylog.com>
2023-08-02 11:38:01 +0200

    Keep win32 beats at version 7.17.12
    Those were the last ones released for win32
    
    We might consider removing win32 support for the sidecar as well.

--- dist/fetch_collectors.sh
+++ dist/fetch_collectors.sh
@@ -2,7 +2,9 @@
 
 ARCHS=( x86 x86_64 )
 FILEBEAT_VERSION=8.8.2
+FILEBEAT_VERSION_32=7.17.12
 WINLOGBEAT_VERSION=8.8.2
+WINLOGBEAT_VERSION_32=7.17.12
 
 # $1: beat name
 # $2: beat operating system
@@ -40,7 +42,12 @@ download_beat()
 
 for ARCH in "${ARCHS[@]}"
 do
-  download_beat "filebeat" "windows" ${FILEBEAT_VERSION} ${ARCH}
-  download_beat "winlogbeat" "windows" ${WINLOGBEAT_VERSION} ${ARCH}
+  #download_beat "filebeat" "linux" ${FILEBEAT_VERSION} ${ARCH}
+  :
 done
 
+download_beat "filebeat" "windows" ${FILEBEAT_VERSION} x86_64
+download_beat "filebeat" "windows" ${FILEBEAT_VERSION_32} x86
+
+download_beat "winlogbeat" "windows" ${WINLOGBEAT_VERSION} x86_64
+download_beat "winlogbeat" "windows" ${WINLOGBEAT_VERSION_32} x86
