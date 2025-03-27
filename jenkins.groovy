bcfd20f281583aacb062cb3c9e7b2a6a5de7ec1e
Bernd Ahlers <bernd@graylog.com>
2025-03-27 15:24:40 +0100

    Use different Docker image to package Sidecar

--- jenkins.groovy
+++ jenkins.groovy
@@ -88,8 +88,9 @@ pipeline
           {
             docker
             {
-              image 'torch/jenkins-fpm-cook:latest'
-              args '-u jenkins:docker'
+              image 'graylog/internal-sidecar-packaging:latest'
+              args '-u jenkins:jenkins'
+              registryCredentialsId 'docker-hub'
               reuseNode true
             }
           }
