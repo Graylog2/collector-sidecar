2491371d57da3edf665e88dd99ca38da26c09c58
Bernd Ahlers <bernd@graylog.com>
2026-01-23 17:56:26 +0100

    Go fmt

--- superv/internal/keen/signals_unix.go
+++ superv/internal/keen/signals_unix.go
@@ -1,7 +1,21 @@
-//go:build !windows
+// Copyright (C)  2026 Graylog, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the Server Side Public License, version 1,
+// as published by MongoDB, Inc.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// Server Side Public License for more details.
+//
+// You should have received a copy of the Server Side Public License
+// along with this program. If not, see
+// <http://www.mongodb.com/licensing/server-side-public-license>.
+//
+// SPDX-License-Identifier: SSPL-1.0
 
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
+//go:build !windows
 
 package keen
 
