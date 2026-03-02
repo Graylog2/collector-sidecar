bbcc6135ffe350bc4b6bbf749055709a92631048
Bernd Ahlers <bernd@graylog.com>
2026-03-02 13:15:06 +0100

    Fix license headers

--- receiver/windowseventlogreceiver/internal/windows/filter_channels.go
+++ receiver/windowseventlogreceiver/internal/windows/filter_channels.go
@@ -1,19 +1,5 @@
-// Copyright (C)  2026 Graylog, Inc.
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the Server Side Public License, version 1,
-// as published by MongoDB, Inc.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// Server Side Public License for more details.
-//
-// You should have received a copy of the Server Side Public License
-// along with this program. If not, see
-// <http://www.mongodb.com/licensing/server-side-public-license>.
-//
-// SPDX-License-Identifier: SSPL-1.0
+// Copyright (C) 2026 Graylog, Inc.
+// SPDX-License-Identifier: Apache-2.0
 
 package windows
 
